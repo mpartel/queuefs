bdc3aede767b46849e84176b5a757ca2c7419801
Martin Pärtel <martin.partel@gmail.com>
2011-05-06 13:04:54 +0300

    added missing license header

--- src/jobqueue_process.c
+++ src/jobqueue_process.c
@@ -1,3 +1,21 @@
+/*
+    Copyright 2011 Martin Pärtel <martin.partel@gmail.com>
+
+    This file is part of queuefs.
+
+    queuefs is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    queuefs is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with queuefs.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "jobqueue.h"
 #include "jobqueue_process.h"
